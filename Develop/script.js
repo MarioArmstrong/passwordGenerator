01f64b88a118a6b93e4d4791315f507fb718c94b
MarioArmstrong <arms.mario@icloud.com>
2022-07-05 20:56:16 -0800

    second commit - not finished

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
+var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789.,?!@#$%^&*()_+=-{}";
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var password = generatePassword() {
+   let str = "";  
+    for (let i=0; i < length; i++){
+    str += chars.charAt(Math.floor(Math.random() * chars.length));
+};
+console.log(str);
+  str.value = password;
 
-  passwordText.value = password;
+  console.log(password);
 
-}
+}};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
