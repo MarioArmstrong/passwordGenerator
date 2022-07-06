b570d91804381fe611eff4e69c0ff4c857ab200f
MarioArmstrong <arms.mario@icloud.com>
2022-07-05 21:03:32 -0800

    upated readme

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,69 @@
 // Assignment Code
+var passwordHtml = document.getElementById("password");
 var generateBtn = document.querySelector("#generate");
-var passwordText = document.querySelector("#password");
-var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789.,?!@#$%^&*()_+=-{}";
+var passwordLengthAnswer = prompt("how many characters long?");
+var password ="";
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword() {
-   let str = "";  
-    for (let i=0; i < length; i++){
-    str += chars.charAt(Math.floor(Math.random() * chars.length));
+function passwordLength(){
+  console.log("passwordLengthAnswer() is running!");
+
+  if(passwordLengthAnswer === "") {
+      alert("Please provide a valid submission");
+  } else{
+    passwordLength();
+  };
 };
-console.log(str);
-  str.value = password;
 
-  console.log(password);
-
-}};
-
+function genRandomPassword(event) {
+  console.log("genRandomPassword() is running!");
+  var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()-_=+.?';
+  for (let i=0; i < passwordLengthAnswer; i++){
+      password += chars.charAt(Math.floor(Math.random() * chars.length)); //chars.length means the length of the strong aka the characters I listed
+  }                                                                       //charsAt gets the character at a specific point
+      passwordHtml.value = password; //this takes the new value of password and injects it into the variable password
+      console.log(password);
+};  
+  
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", genRandomPassword(event));
+
+
+
+
+
+
+
+
+
+//This is made up code to create a simple password generator:
+
+// var passwordHtml = document.getElementById("password");
+// var passwordLengthAnswer = prompt("how many characters long?");
+// var password = "";
+// // var anotherPasswordAttempt = confirm("Would you like to create another password?");
+
+// function passwordLength(){
+//     console.log(passwordLengthAnswer);
+//     if(passwordLengthAnswer === ""){
+//         alert("Please provide a valid submission");
+//     }
+
+// };
+
+
+// function genRandomPassword() {
+//     var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()-_=+.?';
+//     for (let i=0; i < passwordLengthAnswer; i++){
+//         password += chars.charAt(Math.floor(Math.random() * chars.length)); //chars.length means the length of the strong aka the characters I listed
+//     }                                                                       //charsAt gets the character at a specific point
+// document.getElementById("password").value = password; //this takes the new value of password and injects it into the variable password
+// console.log(password);
+
+// passwordHtml.textContent = password;
+
+// };
+
+
+// passwordLength();
+// genRandomPassword();
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # passwordGenerator
 
-GitHub deploy link:
+GitHub deploy link: https://marioarmstrong.github.io/passwordGenerator/
+
+The password generator is not finished. It will have to be resubmmited for a better grade later. Currently it prompts you with the question "how many characters do you want?", which in turn creates a password according to your response. The downside is I haven't been able to get the button to create the prompt to pop up, so the prompt pops up when you first acces the page.
+
+Also, I only have one question prompt on the page so the user cannot tailor their password beyond the amount of characters.
